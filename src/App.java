f1526f1216133aed3f0f6e39ba842941eaf2da6c
Abdulaziz Alghamdi <aalghamdi2952@stu.kau.edu.sa>
2022-10-02 13:41:38 +0300

    edit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("Hello World");
         int x = 200 ;
         int y = 25;
         int z = x*y;
